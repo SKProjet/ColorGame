eee59509cd1b213aa15fc8024de9b9163be2d6a1
Sandirane Kumarasamy <skumarasamy@corp.aliznet.fr>
2020-12-03 15:10:18 +0100

    '3_menu'

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {PlayerService} from '../player/player.service';
 
 @Component({
   selector: 'app-index',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  connect = false;
+
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit(): void {
+    this.playerService.player$
+      .subscribe(player => {
+        if (player){
+          this.connect = true;
+        }});
   }
 
 }
